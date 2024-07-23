a24b7d54703a912b4580bb6b4bc63bd6caef1282
kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2024-07-23 12:56:40 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="border rounded-[4px] bg-[#AEE9D1] mb-3 text-black px-1 text-[12px] w-[65px] font-medium h-[20px] mt-[4.5px] ml-4">
+        My Tailwind Component
+      </div>
+    </>
+  )
+}
+
+export default App
