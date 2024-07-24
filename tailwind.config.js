a9390f689d6996b247a932f22e6dd9986bed7a35
kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2024-07-24 16:48:24 +0200

    add key frame animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,8 +12,33 @@ export default {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      keyframes: {
+        'slide-in': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateX(8px)',
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateX(0)',
+          },
+        },
+        'slide-up': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(20px)',
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)',
+          },
+        },
+      },
+      animation: {
+        'slide-in': 'slide-in 1s ease-out forwards',
+        'slide-up': 'slide-up 1s ease-out forwards',
+      },
     },
   },
   plugins: [],
 }
-
