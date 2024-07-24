ac01183ef994129fda23fdc0cdf461a93905cefa
kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2024-07-24 16:51:16 +0200

    add about

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Hero from './components/Hero'
 import Header from './components/Header'
 import Work from './components/Work'
+import About from './components/About'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Hero />
       <Work />
+      <About />
     </>
   )
 }
