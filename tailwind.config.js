efb5eaff9ca0614b102219220bac79cb0935ab93
kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2024-07-24 10:20:41 +0200

    configure tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,10 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       fontFamily: {
