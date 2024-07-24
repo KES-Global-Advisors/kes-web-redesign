56276284bdba445cfbc55c53a74de7d31ec292cd
kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2024-07-24 10:21:49 +0200

    add components to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import './App.css'
+import Hero from './components/Hero'
+import Header from './components/Header'
+import Work from './components/Work'
 
 function App() {
 
   return (
     <>
-      <div className="border rounded-[4px] bg-[#AEE9D1] mb-3 text-black px-1 text-[12px] w-[65px] font-medium h-[20px] mt-[4.5px] ml-4">
-        My Tailwind Component
-      </div>
+      <Header />
+      <Hero />
+      <Work />
     </>
   )
 }
