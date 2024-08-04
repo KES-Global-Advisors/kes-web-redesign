57d03bd9b2b6a2fb8617a4c94769ff13698cdc01
kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2024-08-04 20:39:44 +0200

    update about image

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from 'react';
-import glass from '../assets/glass-office.jpg'
-
+// import glass from '../assets/glass-office.jpg'
+// https://i.postimg.cc/mgvKj4t6/Adobe-Stock-813074427.jpg
 const About = () => {
   const sectionRef = useRef(null);
 
@@ -78,7 +78,7 @@ const About = () => {
         <div className="-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
           <img
             alt=""
-            src={glass}
+            src="https://i.postimg.cc/wTXgDQCf/Exp-Capture-3.jpg"
             className="w-[48rem] max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]"
           />
         </div>
