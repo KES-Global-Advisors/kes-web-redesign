d285adba176d48ae49fb2073ca4b539dee91a4b1
kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2024-08-05 19:33:17 +0200

    create and add new sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
-import Hero from './components/Hero'
-import Header from './components/Header'
-import Work from './components/Work'
+// import Hero from './components/Hero'
+// import Header from './components/Header'
+// import Work from './components/Work'
+import SlideGrid from './components/SlideGrid'
 import About from './components/About'
 import Testimonials from './components/Testimonials'
 import Footer from './components/Footer'
 import Contact from './components/Contact'
+import HeaderSection from './components/HeaderSection'
+import HeroSection from './components/HeroSection'
+import ApproachSection from './components/ApproachSection'
 
 function App() {
 
   return (
     <>
-      <Header />
-      <Hero />
-      <Work />
+      {/* <Header /> */}
+      {/* <Hero /> */}
+      <HeaderSection />
+      <HeroSection />
+      <ApproachSection />
+      <SlideGrid />
+      {/* <Work /> */}
       <About />
       <Testimonials />
       <Contact />
