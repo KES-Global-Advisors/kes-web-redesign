e1f26f898702f1584120e41845cf4be5db49ccb7
kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2024-08-09 10:03:35 +0200

    import components

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,24 @@
-// import Hero from './components/Hero'
-// import Header from './components/Header'
-// import Work from './components/Work'
-import SlideGrid from './components/SlideGrid'
-import About from './components/About'
+import Hero from './components/Hero'
+import Header from './components/Header'
 import Testimonials from './components/Testimonials'
+import Approach from './components/Approach'
+import Work from './components/Work'
+import About from './components/About'
+import WhitePapers from './components/WhitePapers'
 import Footer from './components/Footer'
 import Contact from './components/Contact'
-import HeaderSection from './components/HeaderSection'
-import HeroSection from './components/HeroSection'
-import ApproachSection from './components/ApproachSection'
 
 function App() {
 
   return (
     <>
-      {/* <Header /> */}
-      {/* <Hero /> */}
-      <HeaderSection />
-      <HeroSection />
-      <ApproachSection />
-      <SlideGrid />
-      {/* <Work /> */}
-      <About />
+      <Header />
+      <Hero />
       <Testimonials />
+      <Approach />
+      <Work />
+      <About />
+      <WhitePapers />
       <Contact />
       <Footer />
     </>
