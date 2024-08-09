1c21e2f55c865d16472ae6d63f263d6b6c7b40cb
kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2024-08-09 09:59:46 +0200

    add swiping to testimonials

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -1,114 +1,104 @@
-// const Testimonials = () => {
-//   return (
-//     <section className="relative overflow-hidden lg:bg-black bg-white px-6 py-24 sm:py-32 lg:px-8">
-//       <div className="absolute inset-0 -z-10 bg-[radial-gradient(45rem_50rem_at_top,theme(colors.indigo.100),white)] opacity-20" />
-//       <div className="absolute inset-y-0 right-1/2 -z-10 mr-16 w-[200%] origin-bottom-left skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:mr-28 lg:mr-0 xl:mr-16 xl:origin-center" />
-//       <div className="mx-auto max-w-2xl lg:max-w-4xl">
-//         <img alt="" src="https://tailwindui.com/img/logos/workcation-logo-indigo-600.svg" className="mx-auto h-12" />
-//         <figure className="mt-10">
-//           <blockquote className="text-center text-xl font-semibold leading-8 lg:text-white text-gray-900 sm:text-2xl sm:leading-9">
-//             <p>
-//               “Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo expedita voluptas culpa sapiente alias
-//               molestiae. Numquam corrupti in laborum sed rerum et corporis.”
-//             </p>
-//           </blockquote>
-//           <figcaption className="mt-10">
-//             <img
-//               alt=""
-//               src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-//               className="mx-auto h-10 w-10 rounded-full"
-//             />
-//             <div className="mt-4 flex items-center justify-center space-x-3 text-base">
-//               <div className="font-semibold lg:text-white text-gray-900">Judith Black</div>
-//               <svg width={3} height={3} viewBox="0 0 2 2" aria-hidden="true" className="fill-gray-900">
-//                 <circle r={1} cx={1} cy={1} />
-//               </svg>
-//               <div className="lg:text-white text-gray-600">CEO of Workcation</div>
-//             </div>
-//           </figcaption>
-//         </figure>
-//       </div>
-//     </section>
-//   )
-// }
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { Navigation, Pagination, Mousewheel, Keyboard } from 'swiper/modules';
+import '../styles.css';
 
-// export default Testimonials;
-
-import React, { useState } from 'react';
-import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
-
-interface Testimonial {
-  id: number;
-  text: string;
-  author: string;
-  position: string;
-  imageUrl: string;
-}
-
-const testimonials: Testimonial[] = [
+const testimonials = [
   {
     id: 1,
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo expedita voluptas culpa sapiente alias molestiae. Numquam corrupti in laborum sed rerum et corporis.",
-    author: "Judith Black",
-    position: "CEO of Workcation",
-    imageUrl: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    text: `
+          "Kevin has been a trusted advisor for me for over 15 years.
+          He and his teams have helped facilitate a number of employee strategic programs for me over the years and to date,
+          every one of those programs has not only delivered on the original goals but have been sustained over time.
+          Most recently Kevin and his team have helped Marathon implement a corporate wide,
+          employee led strategic improvement effort for refining that is truly owned by our employees and delivering real value to all aspects of our business."
+          `,
+    author: "Kevin Bogard",
+    position: "SVP Refining, Marathon Petroleum",
   },
   {
     id: 2,
-    text: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.",
-    author: "John Doe",
-    position: "CTO of Example Co.",
-    imageUrl: "https://images.unsplash.com/photo-1503264111161-8cda51c5f93a?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    text: `
+          "I strongly recommend working with Kevin and his KES Global Advisors company.
+          My experience as a client working together with Kevin and his team has been unique.
+          The capacity and strategy of Kevin to fully let the client realize its goals is impressive.
+          Immediate results are there showing benefits for the companies as implementation take place.
+          Best practices is the daily thinking."
+          `,
+    author: "Rubén Vásquez",
+    position: "Supervisor at Halliburton",
   },
-  // Add more testimonials as needed
+  {
+    id: 3,
+    text: `
+          "KES is a trusted and valued partner for SPP.
+          Over the past 4 years, they have supported a fundamental change in how we develop and execute our site strategy.
+          Changing from a top down to bottoms up ownership of our strategic initiative has been a game changer.
+          This approach, guided by KES, fortified our strategic initiative by leveraging the knowledge, ideas, and energy of all employees, not just a select few leaders.
+          The resulting alignment and ownership we see across the site supports our continued growth and improvement."
+          `,
+    author: "Holly Jackson",
+    position: "GM, Marathon Petroleum",
+  },
+  {
+    id: 4,
+    text: `
+          "For several years, KES has consistently demonstrated an ability to facilitate discussion on strategy which led to successful results with vision,
+          strategy and tactics for SPP. KES possesses a unique talent to guide discussion to coalesce what are often complex problem statements and distill ideas into cultivated solutions."
+          `,
+    author: "Josh Mallonee",
+    position: "Product Control Manager, Marathon Petroleum",
+  },
+  {
+    id: 5,
+    text: `
+          "I have worked with KES professionals across multiple Marathon Petroleum sites and have been witness to the
+          individualized approach they take to understand each site's key strategic opportunities,
+          and focus the local teams on evaluating and overcoming hurdles to achieving their potential.
+          The ground-up methodology taken by KES ensures ownership of the vision, tactics and strategies
+          from the individual contributor to the GM, long after KES has completed their part of the engagement."
+          `,
+    author: "Bob Lawrence",
+    position: "Controller Refining Division, Marathon Petroleum ",
+  },
 ];
 
 const Testimonials: React.FC = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  const handlePrev = () => {
-    setCurrentIndex((prevIndex) => (prevIndex === 0 ? testimonials.length - 1 : prevIndex - 1));
-  };
-
-  const handleNext = () => {
-    setCurrentIndex((prevIndex) => (prevIndex === testimonials.length - 1 ? 0 : prevIndex + 1));
-  };
-
-  const currentTestimonial = testimonials[currentIndex];
-
   return (
-    <section className="relative overflow-hidden lg:bg-black bg-white px-6 py-24 sm:py-32 lg:px-8">
-      <div className="absolute inset-0 -z-10 bg-[radial-gradient(45rem_50rem_at_top,theme(colors.indigo.100),white)] opacity-20" />
-      <div className="absolute inset-y-0 right-1/2 -z-10 mr-16 w-[200%] origin-bottom-left skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:mr-28 lg:mr-0 xl:mr-16 xl:origin-center" />
-      <div className="mx-auto max-w-2xl lg:max-w-4xl">
-        <img alt="" src="https://tailwindui.com/img/logos/workcation-logo-indigo-600.svg" className="mx-auto h-12" />
-        <figure className="mt-10 transition-opacity duration-500 ease-in-out">
-          <blockquote className="text-center text-xl font-semibold leading-8 lg:text-white text-gray-900 sm:text-2xl sm:leading-9">
-            <p>{currentTestimonial?.text}</p>
-          </blockquote>
-          <figcaption className="mt-10">
-            <img
-              alt={currentTestimonial?.author}
-              src={currentTestimonial?.imageUrl}
-              className="mx-auto h-10 w-10 rounded-full"
-            />
-            <div className="mt-4 flex items-center justify-center space-x-3 text-base">
-              <div className="font-semibold lg:text-white text-gray-900">{currentTestimonial?.author}</div>
-              <svg width={3} height={3} viewBox="0 0 2 2" aria-hidden="true" className="fill-gray-900">
-                <circle r={1} cx={1} cy={1} />
-              </svg>
-              <div className="lg:text-white text-gray-600">{currentTestimonial?.position}</div>
-            </div>
-          </figcaption>
-        </figure>
-        <div className="flex justify-between mt-4">
-          <button onClick={handlePrev} className="bg-gray-200 rounded-full p-2 hover:bg-gray-300">
-            <FaArrowLeft />
-          </button>
-          <button onClick={handleNext} className="bg-gray-200 rounded-full p-2 hover:bg-gray-300">
-            <FaArrowRight />
-          </button>
-        </div>
+    <section className="relative overflow-hidden lg:bg-blue-700 bg-white px-6 py-24 sm:py-32 lg:px-8">
+      <div className="mx-auto max-w-2xl lg:max-w-6xl">
+      <h2 className="text-center font-semibold leading-7 text-indigo-600 lg:hidden">Testimonials</h2>
+        <Swiper
+          cssMode={true}
+          navigation={true}
+          pagination={true}
+          mousewheel={true}
+          keyboard={true}
+          modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+          className="mySwiper"
+        >
+          {testimonials.map((testimonial) => (
+            <SwiperSlide key={testimonial.id}>
+              <figure className="mt-10 transition-opacity duration-500 ease-in-out lg:min-h-[400px] min-h-[520px]">
+                <blockquote className="text-center font-semibold leading-8 lg:text-white text-gray-900 sm:text-2xl sm:leading-9">
+                  <p>{testimonial.text}</p>
+                </blockquote>
+                <figcaption className="mt-10">
+                  <div className="mt-4 flex items-center justify-center space-x-3 text-base">
+                    <div className="font-semibold lg:text-white text-gray-900">{testimonial.author}</div>
+                    <svg width={3} height={3} viewBox="0 0 2 2" aria-hidden="true" className="fill-gray-900">
+                      <circle r={1} cx={1} cy={1} />
+                    </svg>
+                    <div className="lg:text-white text-gray-600">{testimonial.position}</div>
+                  </div>
+                </figcaption>
+              </figure>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </section>
   );
