099f131d3954783f4d6ef4f29238521a694fb0e5
kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2024-08-11 17:42:01 +0200

    add globe water mark to contact

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -32,7 +32,19 @@ const Contact = () =>  {
 
   return (
     <Element name="contact">
-      <div ref={sectionRef} className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
+      <div 
+        ref={sectionRef}
+        className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8"
+        style={{
+          backgroundImage: "url('/src/assets/KES-Globe.png')",
+          backgroundAttachment: 'fixed',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: '45%',
+          backgroundBlendMode: 'overlay',
+          backgroundColor: 'rgba(255, 255, 255, 0.8)', // This adds a semi-transparent white overlay
+        }}
+      >
         <div className="mx-auto max-w-2xl text-center">
           <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Contact Us</h2>
         </div>
