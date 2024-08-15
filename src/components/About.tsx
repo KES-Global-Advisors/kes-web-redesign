d15289eab0f06b4c66bab8dec583c4e82e02639e
kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2024-08-15 19:48:05 +0200

    swap images

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -58,7 +58,7 @@ const About = () => {
           <div className="-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden hidden lg:block">
             <img
               alt="Experience Capture by KES Global Advisors"
-              src="https://i.postimg.cc/KYvcZsjd/KES-Global-About-Us-10-2.avif"
+              src="https://i.postimg.cc/qRJZNy36/About-Us-Banner.avif"
               className="w-[48rem] max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]"
             />
           </div>
