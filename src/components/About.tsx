675465ab75aa426d6c32718ae6e2c1e87ed71d05
kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2024-08-26 13:38:01 +0200

    fix spacing

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -59,7 +59,7 @@ const About = () => {
             />
           </div>
 
-          <div className="lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
+          <div className="mt-[-35px] lg:mt-[-15px] lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
             <div className="lg:pr-4">
               <div className="max-w-xl text-base leading-7 text-gray-700 lg:max-w-lg">
                 <p>
